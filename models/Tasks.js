608c28726da2e589c1c3e6d72d26439100a8bcbe
WarriorGoat <michael.d.spitz@gmail.com>
2023-02-21 11:30:18 -0500

    Final revisions

--- models/Tasks.js
+++ models/Tasks.js
@@ -1,24 +1,28 @@
 //import mongoose library
-const mongoose = require ("mongoose");
-const {Schema} = mongoose;
-const {v4: uuidv4} = require("uuid");
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+const { v4: uuidv4 } = require("uuid");
 
 //Create a blogSchema
 const taskSchema = new Schema({
-    id: { type: String, required: true, default: () => uuidv4() },
-    name: {type: String, required: true },
-    assignee: {type: String, required: true},
-    description: {type: String, required: true},
-    comments: [{ body: String, date: Date }],
-    completed: {type: Boolean, default: false},
-    dateCreated: {type: Date, default: Date.now, required: true},
-    dateDue: {type: Date, default: Date.now, required: true},
-    dateCompleted: {type: Date, default: ''},
-    status: {type: String, default: 'incomplete', enum: ['incomplete', 'complete', 'deferred']}
+  id: { type: String, required: true, default: () => uuidv4() },
+  name: { type: String, required: true },
+  assignee: { type: String, required: true },
+  description: { type: String, required: true },
+  comments: [{ body: String, date: Date }],
+  completed: { type: Boolean, default: false },
+  dateCreated: { type: Date, default: Date.now, required: true },
+  dateDue: { type: Date, default: Date.now, required: true },
+  dateCompleted: { type: Date, default: "" },
+  status: {
+    type: String,
+    default: "incomplete",
+    enum: ["incomplete", "complete", "deferred"],
+  },
 });
 
 //Register model to the database collection
-const tasksModel = mongoose.model('tasks', taskSchema);
+const tasksModel = mongoose.model("tasks", taskSchema);
 
 //Make the model available to other files
 module.exports = tasksModel;
--- routes/tasks.js
+++ routes/tasks.js
@@ -1,14 +1,14 @@
-const express = require('express');
+const express = require("express");
 const taskRoutes = express.Router();
-const { v4: uuidv4 } = require ("uuid");
-const taskController = require('../controllers/taskController');
+const { v4: uuidv4 } = require("uuid");
+const taskController = require("../controllers/taskController");
 uuidv4();
 
 /* GET all tasks. */
-taskRoutes.get('/all', taskController.getAllTasks);
+taskRoutes.get("/all", taskController.getAllTasks);
 
 // This section will pull a single record, using the a dynamic id paramter.
-taskRoutes.get('/get-one/:id', taskController.getOneTask);
+taskRoutes.get("/get-one/:id", taskController.getOneTask);
 
 //Add a new post
 taskRoutes.post("/create-one", taskController.createNewTask);
@@ -17,12 +17,12 @@ taskRoutes.post("/create-one", taskController.createNewTask);
 taskRoutes.post("/create-many", taskController.createManyTasks);
 
 // This section will help you delete a single record by id.
-taskRoutes.delete('/delete-one/:id', taskController.deleteOneTask); 
+taskRoutes.delete("/delete-one/:id", taskController.deleteOneTask);
 
 //Mark a task as complete
-taskRoutes.put('/complete-one/:id', taskController.completeTask);
+taskRoutes.put("/complete-one/:id", taskController.completeTask);
 
 // Delete all records marked as complete
-taskRoutes.delete('/delete-complete', taskController.deleteComplete);
+taskRoutes.delete("/delete-complete", taskController.deleteComplete);
 
 module.exports = taskRoutes;
